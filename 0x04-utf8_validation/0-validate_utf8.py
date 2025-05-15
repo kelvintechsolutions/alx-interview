242f1e13adca31ac7d17dcdb4d289e40fb44b037
kelvintechsolutions <kevohmutugi51@gmail.com>
2025-05-15 15:02:43 +0000

    updates

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,43 +2,29 @@
 """UTF-8 validation module.
 """
 
-
 def validUTF8(data):
-    """Checks if a list of integers are valid UTF-8 codepoints.
-    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
-    """
-    if not data:
-        return True
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+    num_bytes = 0
 
-    i = 0
-    while i < len(data):
-        if data[i] <= 0x7F:
-            # ASCII character (1 byte)
-            i += 1
-        elif data[i] <= 0xDF:
-            # Invalid: should be 2-byte sequence but only 1 byte provided
-            return False
-        elif data[i] <= 0xE0:
-            # 2-byte sequence
-            if i + 1 >= len(data) or data[i + 1] < 0x80 or data[i + 1] > 0xBF:
+    for byte in data:
+        byte = byte & 0xFF  # Consider only the least significant 8 bits
+
+        if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                # If it starts with 1 but doesn't match any valid prefix
                 return False
-            i += 2
-        elif data[i] <= 0xEF:
-            # 3-byte sequence
-            if i + 2 >= len(data) or data[i + 1] < 0x80 or data[i + 1] > 0xBF or data[i + 2] < 0x80 or data[i + 2] > 0xBF:
-                return False
-            i += 3
-        elif data[i] <= 0xF4:
-            # 4-byte sequence
-            if i + 3 >= len(data) or data[i + 1] < 0x80 or data[i + 1] > 0xBF or data[i + 2] < 0x80 or data[i + 2] > 0xBF or data[i + 3] < 0x80 or data[i + 3] > 0xBF:
-                return False
-            # Check if the codepoint is within the valid range for a 4-byte sequence
-            codepoint = ((data[i] & 0x07) << 18) | ((data[i + 1] & 0x3F) << 12) | ((data[i + 2] & 0x3F) << 6) | (data[i + 3] & 0x3F)
-            if codepoint > 0x10FFFF or (codepoint >= 0xD800 and codepoint <= 0xDFFF):
-                return False
-            i += 4
         else:
-            # Invalid: should not be more than 4 bytes
-            return False
+            # Check that the byte starts with '10'
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
 
-    return True
+    return num_bytes == 0
+
