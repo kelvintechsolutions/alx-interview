5c91081c1cb649b2fe605486843bad6be10dcca2
elitebook <127598846+kelvintechsolutions@users.noreply.github.com>
2025-05-29 18:18:50 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,61 +1,17 @@
+#!/usr/bin/node
+
 const request = require('request');
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: node 0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  
-  if (response.statusCode !== 200) {
-    console.error(`Error: Received status code ${response.statusCode}`);
-    return;
-  }
-
-  try {
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
-    const characterNames = [];
-    
-    let index = 0;
-    const fetchNextCharacter = () => {
-      if (index >= characterUrls.length) {
-        characterNames.forEach(name => console.log(name));
-        return;
-      }
-      
-      request(characterUrls[index], (err, res, charBody) => {
-        if (err) {
-          console.error('Error:', err);
-          return;
-        }
-        
-        if (res.statusCode !== 200) {
-          console.error(`Error: Received status code ${res.statusCode} for ${characterUrls[index]}`);
-          return;
-        }
-        
-        try {
-          const characterData = JSON.parse(charBody);
-          characterNames.push(characterData.name);
-          index++;
-          fetchNextCharacter();
-        } catch (parseError) {
-          console.error('Error parsing character data:', parseError);
-        }
-      });
-    };
-    
-    fetchNextCharacter();
-  } catch (parseError) {
-    console.error('Error parsing film data:', parseError);
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
